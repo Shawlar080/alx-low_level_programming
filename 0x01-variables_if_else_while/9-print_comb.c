16126b07cc5d12b8a171a27d52170e726ad7a826
Shawlar080 <oyewoleshola5@gmail.com>
2022-09-12 03:27:12 -0700

    prints all possible combinations of single-digit numbers

--- 0x01-variables_if_else_while/9-print_comb.c
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -1,9 +1,9 @@
 #include <stdio.h>
-
 /**
- * main - Prints a series of numbers with commas
- *
- * Return: Always (Success);
+ * main - Prints series of numbers separated by commas
+ * Description: using the main function
+ * this program prints "0,1,2,3,4,5,6,7,8,9"
+ * Return: 0
  */
 int main(void)
 {
@@ -11,15 +11,13 @@ int main(void)
 
 	for (c = '0'; c <= '9'; c++)
 	{
-		putchar(c);
-
-		if (c != '9')
-		{
-			putchar(',');
-			putchar('');
-		}
-	}
-
-	putchar('\n');
-	return (0);
+	putchar(c);
+if (c != '9')
+{
+putchar(',')
+putchar(' ');
+}
+}
+putchar('\n');
+return (0);
 }
